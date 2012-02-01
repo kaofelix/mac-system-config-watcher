087a960e822c6b7b616d4d26bd79e9e0ecafac67
Kao Félix <kcfelix@gmail.com>
2012-02-01 13:42:30 -0200

    Added some partials to make subprocess calls more readable.

--- detect-and-trigger.py
+++ detect-and-trigger.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 import subprocess
+from functools import partial
 
 SSID_GET="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I | grep ' SSID:' | cut -d ':' -f 2 | tr -d ' '"
 WORK_SSIDS=["twdata", "twdata_test"]
 
+shell = partial(subprocess.call, shell=True)
+shell_out = partial(subprocess.check_output, shell=True)
 
 def change_location(new_location):
-    subprocess.call("growlnotify -n LocationChange "
-                    "-m 'Location changed to {}' "
-                    "New Network Location".format(new_location),
-                    shell=True)
-    subprocess.call("scselect {}".format(new_location), shell=True)
+    shell("growlnotify -n LocationChange "
+          "-m 'Location changed to {}' "
+          "New Network Location".format(new_location))
+    shell("scselect {}".format(new_location))
 
 def main():
-    current_ssid = subprocess.check_output(SSID_GET, shell=True).strip()
+    current_ssid = shell_out(SSID_GET).strip()
     if current_ssid in WORK_SSIDS:
         change_location("ThoughtWorks")
     else:
